b7491502e2c7c9dbf884452653c9e993caa0d109
Eric <erictoribio2010@gmail.com>
2022-01-17 00:43:50 -0500

    pokemon api

---
+++ react/API/pokemon_api/src/components/Pokemon.jsx
@@ -0,0 +1,31 @@
+import React,{useState, useEffect} from 'react'
+
+const Pokemon = () => {
+    const [pokemonNames, setPokemonNames] = useState([])
+    
+        
+    
+    const fetchPokemon=() => {
+   
+            fetch("https://pokeapi.co/api/v2/pokemon?offset=1&limit=807")
+            .then(response => response.json())
+            .then(response => setPokemonNames(response.results))
+            }
+    console.log(pokemonNames.length)
+
+
+    return (
+        <div>
+            <button onClick={fetchPokemon}>Fetch pokemon</button>
+            <div>
+                {pokemonNames.length > 0 && pokemonNames.map((pokemon, idx) =>{
+                    return(
+                        <div key={idx}>{pokemon.name}</div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Pokemon
