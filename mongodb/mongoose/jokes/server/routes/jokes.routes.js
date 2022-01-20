24d638a950860215747faf07639fa5b4ae43dce1
Eric <erictoribio2010@gmail.com>
2022-01-19 22:31:02 -0500

    jokes

---
+++ mongodb/mongoose/jokes/server/routes/jokes.routes.js
@@ -0,0 +1,9 @@
+const JokesController = require('../controllers/jokes.controller')
+
+module.exports = app => {
+    app.get("/api/jokes", JokesController.findAllJokes)
+    app.get("/api/jokes/:id", JokesController.findOneSingleJoke)
+    app.post("/api/jokes/new", JokesController.createNewJoke);
+    app.put("/api/jokes/update/:_id", JokesController.updateExistingJoke);
+    app.delete("/api/jokes/delete/:_id", JokesController.deleteAnExistingJoke)
+}
